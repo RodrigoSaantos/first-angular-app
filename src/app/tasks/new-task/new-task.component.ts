7ba5b31a1783db14f4d79d70c39ce390371f4e75
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2026-01-06 17:24:18 -0300

    Using Directives & Two-Way-Binding

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,13 +1,17 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
   @Output() cancel = new EventEmitter<void>();
+  enteredTitle: string = '';
+  enteredSummary: string = '';
+  enteredDueDate: string = '';
 
   onCancel(): void {
     this.cancel.emit();
